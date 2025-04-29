cd515a0f21c2ff7d38b2ab10059b7a84b6ffca13
Anirudh Kashyap <anirudh.kashyap@oodles.io>
2025-04-30 02:32:09 +0530

    deploy  done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/',
+        target: 'https://help-desk-n6c8.onrender.com/',
         changeOrigin: true,
         
       },
